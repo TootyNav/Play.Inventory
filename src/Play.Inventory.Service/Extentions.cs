1c668c982a74de6af2cec4f331b5b43811ba8362
Navid Shiraj <navid2007@hotmail.com>
2023-09-16 21:37:24 +0100

    add controller for inventory item

--- src/Play.Inventory.Service/Extentions.cs
+++ src/Play.Inventory.Service/Extentions.cs
@@ -5,7 +5,7 @@ namespace Play.Inventory.Service;
 
 public static class Extentions
 {
-    public static InventoryItemDto AsDtos(this InventoryItem item){
+    public static InventoryItemDto AsDto(this InventoryItem item){
         return new InventoryItemDto(item.CatalogItemId, item.Quantity, item.AcquiredDate);
     }
 } 
